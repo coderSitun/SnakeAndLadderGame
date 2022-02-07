a4b9920cb490975eb20036ca9116baa348044ece
Situn <ksaswat007@gmail.com>
2022-02-07 14:10:36 +0000

    defined prototype for Player class

--- src/player/player.hpp
+++ src/player/player.hpp
@@ -1,4 +1,16 @@
 #ifndef __PLAYER
 #define __PLAYER
 
+#include<string>
+
+class Player{
+private:
+    unsigned int playerId;
+    unsigned int playerPosition;
+    std::string playerName;
+public:
+    Player(std::string playerName);
+    std::string getPlayerName();
+};
+
 #endif
